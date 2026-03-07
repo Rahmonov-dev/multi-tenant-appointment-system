cf741b35095f50be215e61d046b33f6c88b7b49b
Baxtiyor <baxtiyorr545@gmail.com>
2026-03-07 19:49:47 +0500

    fixed java version fron21 to 25

--- src/test/java/org/architect/multitenantappointmentsystem/MultiTenantAppointmentSystemApplicationTests.java
+++ src/test/java/org/architect/multitenantappointmentsystem/MultiTenantAppointmentSystemApplicationTests.java
@@ -1,9 +1,13 @@
 package org.architect.multitenantappointmentsystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class MultiTenantAppointmentSystemApplicationTests {
 
     @Test
